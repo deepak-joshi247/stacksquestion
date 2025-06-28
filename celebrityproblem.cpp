7b8dae0b3f49d69956b64624b9358f90dbd45e98
deepak-joshi <deepaksjoshi247@gmail.com>
2025-06-28 13:11:52 +0530

    celebrityproblem.cpp

--- celebrityproblem.cpp
+++ celebrityproblem.cpp
@@ -1,59 +1,40 @@
-#include<bits/stdc++.h>
- bool ifknowns(const vector<vector<int>>& M, int a, int b){
-	if(M[a][b]==1){
-		return true;
-	}
-	else{
-		return false;
-	}
-}
-int findCelebrity(int n) {
- 	vector<vector<int>> M(n, vector<int>(n));
-
-	stack<int> s;
-
-  for(int i=0; i<n; i++){
-		s.push(i);
-	}
-	//finding celeb candidate
-	while(s.size()>1){
-		int a=s.top();
-		s.pop();
-		int b=s.top();
-		s.pop();
-		if(ifknowns(M,a,b)){
-			s.push(b);
-		}
-		else{
-			s.push(a);
-		}
-	}
-	int celebcondidate = s.top();
-	//checking if candidate is celeb
-	int countzero = 0;
-	for(int i=0;i<n;i++){
-		if(M[celebcondidate][i]==0){
-			countzero++;
-		}
-	}
-	bool rowcheck = false;
-	if(countzero == n){
-		rowcheck = true;
-	}
-	int countone=0;
-	for(int i=0;i<n;i++){
-		if(M[i][celebcondidate]==1){
-			countone++;
-		}
-	}
-	bool colcheck = false;
-	if(countone == n-1){
-		colcheck = true;
-	}
-	if(colcheck && rowcheck ){
-		return celebcondidate;
-	}
-	else{
-		return -1;
-	}
-}
+class Solution {
+  public:
+    int celebrity(vector<vector<int> >& mat) {
+        int n = mat.size();
+        int candidate;
+        //using stack
+        stack<int> s;
+        for(int i=0;i<n;i++){
+            s.push(i);
+        }
+        // elimination process
+        while(s.size()>1){
+            int A = s.top();
+            s.pop();
+            int B = s.top();
+            s.pop();
+            if(mat[A][B]==1){
+                //A isn't a celeb
+                s.push(B);
+            }
+            else{
+                s.push(A);
+            }
+        }
+        if(s.empty()){
+            return -1;
+        }
+        candidate = s.top();
+        //final check
+        for(int i=0;i<n;i++){
+            if(candidate == i){
+                continue;
+            }
+            if(mat[i][candidate]==0 || mat[candidate][i]==1){
+                return -1;
+            }
+        }
+        return candidate;
+    }
+};
